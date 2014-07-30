7d69eca9b3622290bbdb65b063c237fca6d6257a
zvenshy <zvenshy@gmail.com>
2014-07-30 17:55:20 +0800

    left_but_a --> left_but

--- application/views/post.php
+++ application/views/post.php
@@ -351,7 +351,7 @@ jQuery(document).ready(function() {
 			})*/
 
                 function right_but(){
-                    $('left_but_a').style.left = '0px';
+                    $('left_but').style.left = '0px';
 					start++;numCount++;
 					if(start+1>count)
 					{
@@ -391,7 +391,7 @@ jQuery(document).ready(function() {
 					start--;numCount--;
                      if(start-1==0)
                         {
-                            $('left_but_a').style.left = '-9999px';
+                            $('left_but').style.left = '-9999px';
                             //return;
                         }
 					resize(size[start-1]);
